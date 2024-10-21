8a309c37ff0b19b5cc68fe68b1dbf7da7c9ccc83
mariadb-pieterhumphrey <pieter.humphrey@mariadb.com>
2024-10-21 06:25:06 -0700

    adding photo-prism-values-pvc.yaml

--- terraform/modules/kdrDemo/cluster.tf
+++ terraform/modules/kdrDemo/cluster.tf
@@ -3,12 +3,12 @@ resource "digitalocean_kubernetes_cluster" "this" {
   region        = "sfo2"
   auto_upgrade  = true
   surge_upgrade = true
-  version       = "1.28.14-do.3"
+  version       = "1.30.5-do.3"
   tags          = [var.environment]
 
   node_pool {
     name       = "worker-pool"
-    size       = "s-2vcpu-4gb"
+    size       = "s-4vcpu-8gb"
     auto_scale = true
     min_nodes  = 1
     max_nodes  = 3
